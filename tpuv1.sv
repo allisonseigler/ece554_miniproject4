308350b05832374c355a991feef313d173096835
Allison Seigler <55610738+allisonseigler@users.noreply.github.com>
2022-09-27 15:29:35 -0500

    create tpuv1 and instantiate modules

---
+++ tpuv1.sv
@@ -0,0 +1,25 @@
+module tpuv1
+  #(
+    parameter BITS_AB=8,
+    parameter BITS_C=16,
+    parameter DIM=8,
+    parameter ADDRW=16;
+    parameter DATAW=64;
+    )
+   (
+    input clk, rst_n, r_w, // r_w=0 read, =1 write
+    input [DATAW-1:0] dataIn,
+    output [DATAW-1:0] dataOut,
+    input [ADDRW-1:0] addr
+   );
+  
+  memA #(.BITS_AB(BITS_AB), .DIM(DIM)) MEM_A(.clk(clk), .rst_n(rst_n), .en(????), .WrEn(???), .Ain(???), .Arow(???), .Aout(???));
+  
+  memB #(.BITS_AB(BITS_AB), .DIM(DIM)) MEM_B(.clk(clk), .rst_n(rst_n), .en(???), .Bin(???), .Bout(???));
+  
+  systolic_array #(.BITS_AB(BITS_AB), .BITS_C(BITS_C), .DIM(DIM)) SYS_ARR(.clk(clk), .rst_n(rst_n), .WrEn(???), .en(???), .A(???), .B(???), .Cin(???), .Crow(???), .Cout(???));
+  
+  
+  
+endmodule
+  
